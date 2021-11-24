73a04603cbeb051b26055405297eb95cf41e65d0
Mohamed amine Grissen <mgrissen@e2r12p6.1337.ma>
2021-11-24 02:01:30 +0100

    qwerty

--- parsing/error.c
+++ parsing/error.c
@@ -6,11 +6,47 @@
 /*   By: mgrissen <mgrissen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 01:01:03 by mgrissen          #+#    #+#             */
-/*   Updated: 2021/11/24 01:01:35 by mgrissen         ###   ########.fr       */
+/*   Updated: 2021/11/24 02:00:47 by mgrissen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+int	print_check_error(char *str)
+{
+	write(2, "minishell: ", 12);
+	write(2, "syntax error near unexpected token `", 36);
+	write(2, str, ft_strlen(str));
+	write(2, "'\n", 3);
+	__get_var(SETEXIT, 258);
+	return (1);
+}
+
+void	nb_of_dq(char *line, int *d_q, int *i)
+{
+	(*d_q) = 1;
+	(*i)++;
+	while (line[*i] && line[*i] != '"')
+		(*i)++;
+	if (line[*i] == '"')
+	{
+		(*d_q) = 0;
+		(*i)++;
+	}
+}
+
+void	nb_of_sq(char *line, int *s_q, int *i)
+{
+	(*s_q) = 1;
+	(*i)++;
+	while (line[*i] && line[*i] != '\'')
+		(*i)++;
+	if (line[*i] == '\'')
+	{
+		(*s_q) = 0;
+		(*i)++;
+	}
+}
 
 int	nb_of_quotes(char *line)
 {
@@ -24,29 +60,9 @@ int	nb_of_quotes(char *line)
 	while (line[i])
 	{
 		if (line[i] == '"')
-		{
-			d_q = 1;
-			i++;
-			while (line[i] && line[i] != '"')
-				i++;
-			if (line[i] == '"')
-			{
-				d_q = 0;
-				i++;
-			}
-		}
+			nb_of_dq(line, &d_q, &i);
 		else if (line[i] == '\'')
-		{
-			s_q = 1;
-			i++;
-			while (line[i] && line[i] != '\'')
-				i++;
-			if (line[i] == '\'')
-			{
-				s_q = 0;
-				i++;
-			}
-		}
+			nb_of_sq(line, &s_q, &i);
 		else
 			i++;
 	}
@@ -62,9 +78,10 @@ int	check_space(char *line, int i)
 {
 	if (line[i] == '|')
 	{
-		while (line[i + 1] == ' ')
+		i++;
+		while (line[i] == ' ')
 			i++;
-		if (line[i + 1] == '|')
+		if (line[i] == '|' || line[i] == '\0')
 			return (1);
 	}
 	return (0);
@@ -74,48 +91,23 @@ int	check_error_pipes(char *line)
 {
 	int	i;
 	int	d_q;
-	int	k;
 
 	d_q = 0;
 	i = 0;
 	while (line[i] == ' ')
 		i++;
 	if (line[i] == '|')
-	{
-		printf("syntax error near unexpected token `|'\n");
-		__get_var(SETEXIT, 258);
-		return (1);
-	}
+		return (print_check_error("|"));
 	while (line[i])
 	{
 		if (line[i] == '"')
 			d_q++;
 		if ((line[i] == '|' && !line[i + 1])
 			|| (check_space(line, i) && d_q % 2 == 0))
-		{
-			__get_var(SETEXIT, 258);
-			printf("syntax error near unexpected token `|'\n");
-			return (1);
-		}
+			return (print_check_error("|"));
 		if ((d_q % 2 == 0 && (line[i] == '<' || line[i] == '>')
 				&& line[i + 1] == '|'))
-		{
-			__get_var(SETEXIT, 258);
-			printf("syntax error near unexpected token `newline'\n");
-			return (1);
-		}
-		if (line[i] == '|')
-		{
-			k = i + 1;
-			while (line[k] == ' ')
-				k++;
-			if (line[k] == '\0')
-			{
-				__get_var(SETEXIT, 258);
-				printf("syntax error near unexpected token `|'\n");
-				return (1);
-			}
-		}
+			return (print_check_error("newline"));
 		i++;
 	}
 	return (0);
@@ -155,31 +147,31 @@ int	check_error_redirections(char *line)
 		if (line[i] == '"')
 			d_q++;
 		if (line[i] == '<' && rederror(line) && (d_q % 2) == 0)
-		{
-			printf("syntax error near unexpected token `newline'\n");
-			__get_var(SETEXIT, 258);
-			return (1);
-		}
+			return (print_check_error("newline"));
 		else if ((line[i] == '<' && line[i + 1] == '<' && line[i + 2] == '<')
 			|| (line[i] == '>' && line[i + 1] == '|'))
-		{
-			printf("syntax error near unexpected token `<'\n");
-			__get_var(SETEXIT, 258);
-			return (1);
-		}
+			return (print_check_error("<"));
 		else if ((line[i] == '>' && line[i + 1] == '<'))
-		{
-			printf("syntax error near unexpected token `<'\n");
-			__get_var(SETEXIT, 258);
-			return (1);
-		}
+			return (print_check_error("<"));
 		else if (line[i] == '>' && rederror(line) && (d_q % 2) == 0)
-		{
-			printf("minishell: syntax error near unexpected token `>'\n");
-			__get_var(SETEXIT, 258);
-			return (1);
-		}
+			return (print_check_error(">"));
 		i++;
+	}
+	return (0);
+}
+
+int	only_quotes(char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i] && line[i] == ' ')
+		i++;
+	if (line[i] && line[i + 1] && line[i] == '"' && line[i + 1] == '"')
+	{
+		printf("minishell: : command not found\n");
+		__get_var(SETEXIT, 127);
+		return (1);
 	}
 	return (0);
 }
@@ -187,7 +179,9 @@ int	check_error_redirections(char *line)
 int	check_error(char *line)
 {
 	(void)line;
-	if (nb_of_quotes(line))
+	if (only_quotes(line))
+		return (1);
+	else if (nb_of_quotes(line))
 		return (1);
 	else if (check_error_pipes(line))
 		return (1);
